11b150c25a82b7b4e43b8902934d96fc9a41e814
alexlueng <alexlueng@icloud.com>
2021-05-07 23:36:17 +0800

    add stack and queue basic

---
+++ stackandqueue.py
@@ -0,0 +1,98 @@
+
+
+class Empty(Exception):
+    pass
+
+
+class ArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+
+    def __init__(self):
+        """Create an empty stack"""
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
+
+def is_match_html(raw):
+    S = ArrayStack()
+    j = raw.find('<')
+    while j != -1:
+        k = raw.find('>', j+1)
+        if k == -1:
+            return False
+        tag = raw[j+1:k]
+        if not tag.startswith('/'):
+            S.push(tag)
+        else:
+            if S.is_empty():
+                return False
+            if tag[1:] != S.pop():
+                return False
+        j = raw.find('<', k+1)
+    return S.is_empty()
+
+
+def ArrayQueue:
+    """FIFO queue implementation useing a Python list as underlying storage."""
+
+    DEFAULT_CAPACITY = 10
+
+    def __init__(self):
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        return self._data[self._front]
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        answer = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+
+    def enqueue(self, e):
+        if self._size == len(self._data):
+            self.resize(2 * len(self._data))
+        avail = (self._front + self._size) % len(self._data) # ?
+        self._data[avail] = e
+        self._size += 1
+
+    def resize(self, cap):
+        old = self._data
+        self._data = [None] * cap
+        walk = self._front
+        for k in range(self._size):
+            self._data[k] = old[k]
+            walk = (1 + walk) % len(old)
+        self._front = 0
+        
+        
