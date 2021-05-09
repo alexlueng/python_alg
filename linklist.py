bd20b9eefd9c3bad073e34c5ed09c30d2edc77af
alexlueng <alexlueng@icloud.com>
2021-05-09 23:38:14 +0800

    add favoriteList

--- linklist.py
+++ linklist.py
@@ -298,4 +298,88 @@ class PositionalList(_DoublyLinkedBase):
 
 
 
+class FavoriteList:
 
+    class _Item:
+        __slots__ = '_value', '_count'
+        def __init__(self, e):
+            self._value = e
+            self._count = 0
+
+    # nonpublic utilities
+
+    def _find_position(self, e):
+        walk = self._data.first()
+        while walk is not None and walk.element()._value != e:
+            walk = self._data.after(walk)
+        return walk
+
+    def _move_up(self, p):
+        """Move item at Positon p earlier in the list baseed on access count"""
+        if p != self._data.first():
+            cnt = p.element()._count
+            walk = self._data.before(p)
+            if cnt > walk.element()._count:
+                while (walk != self._data.first() and cnt > self._data.before(walk).element()._count):
+                    walk = self._data.before(walk)
+                self._data.add_before(walk, self._data.delete(p))
+
+    # public methods
+
+    def __init__(self):
+        self._data = PositionalList()
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def access(self, e):
+        p = self._find_position(e)
+        if p is None:
+            p = self._data.add_last(self._Item(e))
+        p.element()._count += 1
+        self._move_up(p)
+
+    def remove(self, e):
+        p = self._find_position(e)
+        if p is not None:
+            self._data.delete(p)
+
+    def top(self, k):
+        if not 1 <= k <= len(self):
+            raise ValueError('Illegal value for k')
+
+        walk = self._data.first()
+        for j in range(k):
+            item = walk.element()
+            yield item._value
+            item = self._data.after(walk)
+
+
+class FavoriteListMTF(FavoriteList):
+    def _move_up(self, p):
+        if p != self._data.first():
+            self._data.add_first(self._data.delete(p))
+
+    def top(self, k):
+        if not 1 <= k <= len(self):
+            raise ValueError('Illegal value for k')
+        temp = PositionalList()
+        for item in self._data:
+            temp.add_last(item)
+        
+        # We repeatedly find, report, and remove element with largest count
+        for j in range(k):
+            # Find and report next highest from temp
+            highPos = temp.first()
+            walk = temp.after(highPos)
+            while walk is not None:
+                if walk.element()._count > highPos.element()._count:
+                    highPos = walk
+                walk = temp.after(walk)
+
+            # We have found the element with highest count
+            yield highPos.element()._value
+            temp.delete(highPos)
