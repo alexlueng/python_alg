9c290aaa58fd1551558c804bec5acf21cd4b1303
alexlueng <alexlueng@icloud.com>
2021-07-07 01:35:29 +0800

    priority queue

--- tree.py
+++ tree.py
@@ -285,8 +285,59 @@ class ExpressionTree(LinkedBinaryTree):
         if self.is_leaf(p):
             result.append(str(p.element()))
         else:
-            result.appen('(')
+            result.append('(')
             self._parenthesize_recur(self.left(p), result)
             result.append(p.element())
             self._parenthesize_recur(self.right(p), result)
             result.append(')')
+
+    def evaluate(self):
+        return self._evaluate_recur(self.root())
+
+    def _evaluate_recur(self, p):
+        if self.is_leaf(p):
+            return float(p.element())
+
+        else:
+            op = p.element()
+            left_val = self._evaluate_recur(self.left(p))
+            right_val = self._evaluate_recur(self.right(p))
+            if op == '+': return left_val + right_val
+            if op == '-': return left_val - right_val
+            if op == '*': return left_val * right_val
+            else: return left_val / right_val
+
+    
+
+class EulerTour:
+    def __init__(self, tree):
+        self._tree = tree
+
+    def tree(self):
+        return self._tree
+
+    def execute(self):
+        if len(self._tree) > 0:
+            return self._tour(self._tree.root(), 0, [])
+
+    def _tour(self, p, d, path):
+        """
+            p Position of current node being visited
+            d depth of p in the tree
+            path list of indices of children on path from root to p
+        """
+        self._hook_previsit(p, d, path)
+        results = []
+        for c in self.tree.children(p):
+            results.append(self._tour(c, d+1, path))
+            path[-1] += 1
+        path.pop()
+        answer = self._hook_postvisit(p, d, path, results)
+
+        return answer
+
+    def _hook_previsit(self, p, d, path):
+        pass
+
+    def _hook_postvisit(self, p, d, path, results):
+        pass
